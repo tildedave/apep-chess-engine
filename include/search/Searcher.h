266ecc3c2f78941b006f9b40d8f8d1ecff8eac09
Dave King <tildedave@gmail.com>
2010-11-27 11:39:28 -0500

    files I didn't include the first time

---
+++ include/search/Searcher.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Searcher {
+public:
+	Searcher();
+	virtual ~Searcher();
+};
---
+++ src/search/Searcher.cpp
@@ -0,0 +1,10 @@
+#include "search/Searcher.h"
+
+Searcher::Searcher() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Searcher::~Searcher() {
+	// TODO Auto-generated destructor stub
+}
