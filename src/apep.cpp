dc8d045a51930bc14868611d57d1fbfa80a52044
Dave King <dave.king@mailtrust.com>
2010-12-15 13:23:03 -0600

    begin perft refactor

--- src/apep.cpp
+++ src/apep.cpp
@@ -40,24 +40,46 @@ int outputUsage(const po::options_description& desc) {
 }
 
 int main(int argc, char** argv) {
-  po::options_description desc("Allowed options");
   std::string fenFile;
-  desc.add_options()
+
+  po::options_description generic("General Options");
+  generic.add_options()
     ("help", "produce help message")
-    ("tactics", "run tactics test")
     ("random", po::value<int>(), "Random seed")
-    ("file", po::value<std::string>(), "Read EPD file for perft/tactics file")
     ("book", po::value<std::string>()->default_value("bookl.dat"), "Read book from specified file")
-    ("fen", po::value<std::string>(), "Specify FEN on command line")
-    ("timeout", po::value<int>()->default_value(10), "Seconds per move (default value of 10 seconds/move)")
     ("expected", po::value<std::string>()->default_value(""), "Expected solution for FEN");
 
+  po::options_description shared("Shared Options");
+  shared.add_options()
+    ("fen", po::value<std::string>(), "Specify FEN on command line")
+    ("file", po::value<std::string>(), "Read EPD file for perft/tactics file");
+
+  po::options_description tactics("Tactics Options");
+  tactics.add_options()
+    ("tactics", "run tactics test")
+    ("timeout", po::value<int>()->default_value(10), 
+                "Seconds per move (default value of 10 seconds/move)");
+
+  bool verbose;
+  bool divide;
+
+  po::options_description perft("Perft Options");
+  perft.add_options()
+    ("perft", "run perft test")
+    ("verbose", po::bool_switch(&verbose)->default_value(false), 
+                "Output time for move generation, processing, and unprocessing")
+    ("divide", po::bool_switch(&divide)->default_value(false), "Output divide for each move?");
+
+
+  po::options_description cmdline_options;
+  cmdline_options.add(generic).add(shared).add(tactics).add(perft);
+
   po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::store(po::parse_command_line(argc, argv, cmdline_options), vm);
   po::notify(vm);    
 
   if (vm.count("help")) {
-    return outputUsage(desc);
+    return outputUsage(cmdline_options);
   }
 
   if (vm.count("random")) {
@@ -74,7 +96,7 @@ int main(int argc, char** argv) {
 
     if (!vm.count("file")) {
       if (!vm.count("fen")) {
-	return outputUsage(desc);
+	return outputUsage(cmdline_options);
       }
 
       std::string fen = vm["fen"].as<std::string>();
